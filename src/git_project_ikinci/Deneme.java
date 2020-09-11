1aa00dbf0eda337532f28ea0f19cd354307fef6a
Burak <kenpiyr@gmail.com>
2020-09-11 14:18:15 +0300

    ilk commitim

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -3,7 +3,9 @@ package git_project_ikinci;
 public class Deneme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("ikinci git proje");
+		System.out.println("ikinci satýr");
 
 	}
 
